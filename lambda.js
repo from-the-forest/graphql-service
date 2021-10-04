2761219390adae9f00b781f85d2c2bf2dbbcf01f
Joseph Cuffney <josephcuffney@gmail.com>
2021-10-04 18:29:55 -0400

    lambda handler

---
+++ lambda.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const { ApolloServer } = require('apollo-server-lambda')
+const { ApolloGateway } = require('@apollo/gateway')
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core')
+
+// managed federation
+const gateway = new ApolloGateway()
+
+const server = new ApolloServer({
+  gateway,
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground()
+  ]
+})
+
+exports.handler = server.createHandler();
